0651adefa0689d7c66e1fe6aea596fca4c358d50
kiraall <kirill.aaltonen@metropolia.fi>
2025-01-14 23:46:02 +0200

    tehtävät

---
+++ moduuli1/tehtava5.py
@@ -0,0 +1,8 @@
+import math
+
+LUOTI_GRAMMOINA = 13.3
+NAULA_LUOTEINA = 32
+LEIVISKÄ_NAULOINA = 20
+
+leiviskat, naulat, luodit = float(input("Anna leiviskät")), float(input("Anna naulat")), float(input("Anna luodit"))
+
