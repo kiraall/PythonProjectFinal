f9a7819067de18c8d7d4ab36f4a0c64924f0edb0
kirall <kirill.aaltonen@metropolia.fi>
2025-01-17 11:32:13 +0200

    tehtava6

--- moduuli1/tehtava5.py
+++ moduuli1/tehtava5.py
@@ -1,7 +1,18 @@
 import math
-leiviskät, naulat, luodit = input("Anna leiviskät:"),input("Anna naulat:"), input("Anna luodit:")
+leiviskät, naulat, luodit = float(input("Anna leiviskät: ")), float(input("Anna naulat: ")), float(input("Anna luodit: "))
 
 luoti_grammoina = 13.3
 naula_grammoina = 32 * luoti_grammoina
 leiviska_grammoina = 20 * naula_grammoina
+
+kokonaisgrammat = (
+    leiviskät * leiviska_grammoina +
+    naulat * naula_grammoina +
+    luodit * luoti_grammoina
+)
+
+kilogrammat = (kokonaisgrammat / 1000)
+grammat = kokonaisgrammat % 1000
+
+print(f"{kilogrammat} kilogrammaa ja {grammat:.2f} grammaa.")
 
